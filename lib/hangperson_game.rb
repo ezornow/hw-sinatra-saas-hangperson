4050f9384308f3cc3fa4db56aebb6ddc1959088b
ezornow <ezornow@wesleyan.edu>
2016-02-12 19:17:31 -0500

    Update hangperson_game.rb

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,9 +8,70 @@ class HangpersonGame
   # def initialize()
   # end
   
+  
   def initialize(word)
     @word = word
+    @guesses=""
+    @wrong_guesses=""
   end
+  
+  def word
+    @word
+  end
+  
+  def word=(new_word)
+    @word=new_word
+  end
+  
+  def guesses
+    @guesses
+  end
+  
+  def wrong_guesses
+    @wrong_guesses
+  end
+  
+  def guess(attempt)
+    attempt='' if attempt.nil?
+    invalid = ((/[a-zA-z]/=~attempt)==nil)
+    uppercase = /[A-Z]/=~attempt
+    correct = @word.include? (attempt)
+    repeat = (@guesses.include? attempt) || (@wrong_guesses.include? attempt)
+    if (invalid)
+      raise ArgumentError
+    elsif uppercase != nil
+      false
+    elsif repeat
+      false
+    elsif correct
+      @guesses=@guesses+attempt
+    else
+      @wrong_guesses=@wrong_guesses+attempt
+    end
+  end
+  
+  def word_with_guesses
+    display_word=""
+    @word.each_char do |x|
+      if (@guesses.include? x)
+        display_word=display_word+x
+      else
+        display_word=display_word+"-"
+      end
+    end 
+    return display_word
+  end
+  
+  def check_win_or_lose
+    if self.word_with_guesses==word
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
+  end
+  
 
   def self.get_random_word
     require 'uri'
